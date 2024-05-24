ca0b9f9adb991deec2ad8d8d402e1e30d351f7ee
olivier-jnk <oliviertrouvain01@gmail.com>
2024-05-24 08:55:58 +0200

    modifs de style et organisation des dossiers/fichiers

--- views/main.js
+++ views/main.js
@@ -10,3 +10,19 @@ function handicap (){
         selectField.style.display = "none"
     }
 };
+
+// Pour apparition / disparition du contenu dans la barre de recherche quand "focused"
+
+const srch = document.getElementById('search-bar');
+const svg = document.getElementById('search-svg')
+
+srch.addEventListener('focus', () => {
+    svg.style.display = 'none'
+    srch.setAttribute('data-placeholder', srch.getAttribute('placeholder'));
+    srch.setAttribute('placeholder', '');
+});
+
+srch.addEventListener('blur', () => {
+    svg.style.display = 'block'
+    srch.setAttribute('placeholder', srch.getAttribute('data-placeholder'));
+});
